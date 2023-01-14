9447a7a3ca52e7cc5bbe685d4f2dbd361036797b
Aman Murya <m.amanmaurya49@gmail.com>
2023-01-14 22:57:53 +0530

    added more codes

--- connecting to frontend/src/index.js
+++ connecting to frontend/src/index.js
@@ -2,16 +2,19 @@ const express = require('express')
 const path=require("path")
 const app = express()
 const port = 3000
+const hbs=require("hbs");
 
 // builtin middlewares
 
 const staticPath=path.join(__dirname,"../public");
 // Customizing the Views Directory
-const templatePath=path.join(__dirname,"../templates")
+const templatePath=path.join(__dirname,"../templates/views")
+const partialsPath=path.join(__dirname,"../templates/partials")
 
 // to set the view engin 
 app.set("view engine","hbs");
 app.set('views',templatePath)
+hbs.registerPartials(partialsPath)
 // app.use(express.static(staticPath));
 
 app.get('', (req, res) => {
@@ -23,14 +26,23 @@ app.get('', (req, res) => {
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+app.get("/about",(req,res)=>{
+  res.render("about")
+})
+
 app.get('/about', (req, res) => {
   res.send('about!')
 })
+
 app.get('/contact', (req, res) => {
   res.send('contact!')
 })
 app.get('*', (req, res) => {
-  res.send('<h1>error 404</h1>')
+  res.render('<h1>error 404</h1>',{
+    // errorcomment:"404, page not found"
+  })
 })
 
 app.listen(port, () => {
